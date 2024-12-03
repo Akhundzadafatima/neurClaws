49dfc671d10b2c8102680376a4d4e7bb4dc04814
akhundzadafatima45 <axundzadefatime.45@gmail.com>
2024-12-03 15:59:22 +0400

    added

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("hamburger-icon").addEventListener("click", function () {
+    const menu = document.querySelector(".pages");
+    menu.classList.toggle("active"); 
+  });
+  
